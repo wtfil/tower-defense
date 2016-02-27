69157bceecafd34b34692e11164a40e18c30d258
Filatov Evgen <evgen.filatov@gmail.com>
2016-02-27 17:49:17 +0200

    cleanup

--- public/src/core/constants.js
+++
@@ -1,1 +0,0 @@
-export const SEGMENT = 32;
--- test/objects.js
+++ test/objects.js
@@ -1,21 +1,22 @@
 describe('objects', () => {
+
 	var dir = __dirname + '/../public/src/objects';
 	var objects = require(dir);
 	var names = [];
 	Object.keys(objects).forEach(key => {
-		if (key === '__esModule') {
+		if (key === '__esModule' || Array.isArray(objects[key])) {
 			return;
 		}
 		var item = objects[key];
+
 		it(key, () => {
-			item.should.have.property('name');
 			item.should.have.property('width').be.an('number');
 			item.should.have.property('height').be.an('number');
-			item.should.have.property('textures').be.an('array');
+			item.should.have.property('name');
 			expect(names).to.not.include(item.name, `"${item.name}" is not unique`);
 			names.push(item.name);
 		});
-	});
 
+	});
 
 });
